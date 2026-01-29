1c5eaf1c0e2b74642e7343f7e41aefd39c2018ac
CRodgb <charles.b.r@outlook.com>
2026-01-28 19:22:03 -0500

    Update navigation pkg for workspace, update navigation package README with required section.

--- src/my_robot_navigation/README.md
+++ src/my_robot_navigation/README.md
@@ -8,13 +8,25 @@ Contains Nav2 specific config & launch files. Provides Navigation behavior.
 
 **Environment:** Physical & Simulation
 
+## Required Frames/Topics
+### Frames
+* `odom` (global_frame for BOTH costmaps)
+* `base_link` (robot_base_frame & fused_cloud.sensor_frame) 
+### Subscription Topics
+* `fused_cloud` (for fused_cloud.topic, expects PointCloud2 message type)
+* `odom` (controller_server.odom_topic)
+### Publishing Topics
+* `ackermann_cmd` (controller_server.command_topic)
+>**Note:** DONT USE ACKERMANN_CMD, need to change this.
+### TF Transforms
+* `odom` -> `base_link` (robot pose)
+* `base_link` -> `sensor frames`(URDF)
+
 ## Package Structure
 
 ### Launch Files  
 * `nav2.launch.py`
-    * Not properly setup for new workspace
-    * Dependencies partially connected in `package.xml` 
-        * (missing `av_sim_core` dependency)
+    * Providing base functionality
 
 ### Config Files  
 * `nav2_params.yaml`
--- src/my_robot_navigation/launch/nav2.launch.py
+++ src/my_robot_navigation/launch/nav2.launch.py
@@ -1,5 +1,3 @@
-## FIX: must adjust old launch description (paths have changed)
-
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
@@ -11,17 +9,17 @@ from launch.substitutions import LaunchConfiguration
 def generate_launch_description():
     
     # --- 1. Find Paths ---
-    av_sim_core_share_dir = get_package_share_directory('av_sim_core')    # Find path to the packages (av_sim_core) share directory
-    nav2_bringup_share_dir = get_package_share_directory('nav2_bringup')  # Find path to the packages (nav2_bringup) share directory
+    my_robot_navigation_share_dir = get_package_share_directory('my_robot_navigation')    # Find path to the package's (my_robot_navigation) share directory
+    nav2_bringup_share_dir = get_package_share_directory('nav2_bringup')  # Find path to the package's (nav2_bringup) share directory
     
     nav2_params_file = os.path.join(
-        av_sim_core_share_dir, 'config', 'nav2_params.yaml')  # Constructs full path to params file from the av_sim_core share directory
+        my_robot_navigation_share_dir, 'config', 'nav2_params.yaml')  # Constructs full path to params file from the my_robot_navigation share directory
 
     # --- 2. Define Launch Arguments ---
     use_map_server = LaunchConfiguration('use_map_server', default='False')  # Not using traditional static maps, so don't try and preload
     use_amcl = LaunchConfiguration('use_amcl', default='False')              # Not localizing on a static map
 
-    # --- 3. Main NAV2 Bringup ---
+    # --- 3. Launch NAV2 Bringup ---
     # Define the main NAV2 launch file (bringup_launch.py), with custom launch arguments
     nav2_bringup_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -34,41 +32,31 @@ def generate_launch_description():
         }.items(),
     )
 
-    # --- 4. Define and Launch Other Nodes ---
+    # --- 4. Define Nodes ---
     # We DO NOT launch pure_pursuit.py or stop_controller.py, NAV2's controller_server replaces them.
-
-    # --- Spawn Actors Node ---
-    # Spawns all the other cars and pedestrians
-    spawn_actors_node = Node(
-        package='av_sim_core',
-        executable='spawn_actors.py',
-        name='spawn_actors',
-        output='screen'
-    )
     
-    # --- Sensor Fusion Node ---
+    # --- Sensor Fusion Node ---  #UPDATE when sensor_fusion.py working
     # Fuses Lidar + Radar into the /carla/hero/fused_objects topic that the costmaps are subscribed to
-    sensor_fusion_node = Node(
-        package='av_sim_core',
-        executable='sensor_fusion.py',
-        name='sensor_fusion_node',
-        output='screen'
-    )
+    # sensor_fusion_node = Node(
+    #     package='my_robot_localization',
+    #     executable='sensor_fusion.py',
+    #     name='sensor_fusion_node',
+    #     output='screen'
+    # )
 
-    # --- Vision Detector Node ---
+    # --- Vision Detector Node ---  #UPDATE when vision_detector.py working
     # Runs YOLO to detect stop signs
-    vision_detector_node = Node(
-        package='av_sim_core',
-        executable='vision_detector.py',
-        name='vision_detector',
-        output='screen'
-    )
+    # vision_detector_node = Node(
+    #     package='my_robot_perception',
+    #     executable='vision_detector.py',
+    #     name='vision_detector',
+    #     output='screen'
+    # )
 
-    # --- 5. Create the Final Launch Description ---
+    # --- 5. Return Final Launch Description ---
     # Collect all individual nodes and launch actions to be returned for execution
     return LaunchDescription([
         nav2_bringup_launch,
-        spawn_actors_node,
-        sensor_fusion_node,
-        vision_detector_node,
+        #sensor_fusion_node,
+        #vision_detector_node,
     ])
